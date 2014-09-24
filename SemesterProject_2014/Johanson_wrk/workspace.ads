f5ac40fe84c255e9408e6eefb870b791006ea0de
Lars-Arne Larsen <lars-arne@outlook.com>
2014-09-24 15:08:34 +0200

    Fixed libraries

---
+++ SemesterProject_2014/Johanson_wrk/workspace.ads
@@ -0,0 +1,27 @@
+<ADSWorkspace Revision="1" Version="100">
+    <Workspace Name="">
+        <Library Name="Johanson_lib" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_datacmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_standard_layers" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_tlines" />
+        <Library Name="adstechlib" />
+        <Library Name="empro_standard_layers" />
+        <Cell Name="Johanson_lib:JTInd0603all" />
+        <Cell Name="Johanson_lib:JTInd0603high" />
+        <Cell Name="Johanson_lib:JTInd0603low" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="schematic.prf" />
+        <LibraryDefs Name="lib.defs" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+        <Cell Name="Johanson_lib:JTCap0603" />
+    </Workspace>
+</ADSWorkspace>
